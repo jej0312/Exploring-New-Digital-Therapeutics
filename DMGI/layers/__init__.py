8eab35cd66e2f697049dc63773ce8b2d98994db3
jej0312 <49752071+jej0312@users.noreply.github.com>
2022-06-07 10:22:23 +0900

    Delete DMGI directory

--- DMGI/layers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .gcn import GCN
-from .readout import AvgReadout
-from .discriminator import Discriminator
-from .attention import Attention
-
--- DMGI/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .logreg import LogReg
-from .DMGI import DMGI
-from .DGI import DGI
